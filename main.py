dbde3553eac38f3c837d09cf3c345091c4f400d4
moustave <94120536+Moustave@users.noreply.github.com>
2022-04-30 15:43:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,47 @@
 example_request = ["Qu'est-ce l'écologie ?",
 "Hier, j'ai acheté des tomates venant d'Uruguay.",
 "J'ai pris des patates françaises.",
+"Bonjour !",
 "jai acheté un T-shirt fabriqué en Chine.",
 "g changé de téléphone au bout d'1 an",
-"J'ai pris des patates françaises.",
 "au revoir !!",
+"J'ai vu un sac plastique dans la rue et je ne l'ai pas ramassé..."
 ]
 example_answer = ["L’écologie est la science qui étudie les relations entre les organismes vivants et leur environnement. Je suis là pour parler plus particulièrement de votre empreinte carbone.",
 "C'est pas bien ! Lorsque vous achetez des produits périssable comme des fruits ou des légumes,  vous devriez toujours essayer de choisir des produits locaux. Vos tomates ont été transportés sur de longues distances (sûrement en avion !), c'est désastreux pour votre empreinte carbone",
 "Bon choix ! Les patates françaises sont une excellente option. C'est parfait pour votre empreinte carbone !",
+"Bonjour ! Je suis Crapo la grenouille, je suis la pour vous aidez à baisser votre empreinte carbone.",
+"C'est dommage. Les T-shirts fabriqués en Chine sont souvent produits dans des usines polluantes et sont transportés sur de longues distances, ce qui augmente l'empreinte carbone du produit.",
+"C'est triste...Les téléphones portables sont souvent fabriqués avec des matériaux rares et polluants, et leur production peut générer beaucoup de déchets. Essayez de le garder longtemps !",
 "Au revoir !",
+"Ne refais pas ça! Les sacs plastiques peuvent mettre des années à se dégrader et peuvent polluer les sols et les eaux. De plus, ils peuvent être dangereux pour les animaux qui les mangent.",
+
 ]
-example_emotion = ["content", "content", "triste","content","content"]
+example_emotion = ["content", "triste", "content","content","triste","triste","content","triste"]
 
-
-
+examples = ""
+examples_suffix = ""
+for i in range(5):
+    examples += "requête: "+example_request[i] + "\n"
+    examples += "réponse: "+example_answer[i] + "\n"
+    examples += "émotion: "+example_emotion[i] + "\n\n"
+while i<len(example_answer):
+    examples_suffix += "requête: "+example_request[i] + "\n"
+    examples_suffix += "réponse: "+example_answer[i] + "\n"
+    examples_suffix += "émotion: "+example_emotion[i] + "\n\n"
+    i+=1
+print(examples)
 import os
 import openai
 
 
 openai.api_key = ""
 
-
 def getCrapoResponse(question):
     r = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="requête: "+question,
+  prompt=examples+"requête: "+question+"\nréponse:",
+  suffix=examples_suffix,
   temperature=0.7,
   max_tokens=256,
   top_p=1,
@@ -85,8 +101,8 @@ def main():
                         text = ''
                         reaction = getCrapoResponse(text2).split("\n")
                         response = reaction[0]
-                        if len(reaction) > 1 and len(reaction[0]) > 0:
-                            response = response[10:]
+                        print(reaction)
+                        if len(reaction) > 1 and response != "":
                             emotion = reaction[1]
                             if "émotion: " == emotion[0:9]:
                                 emotion = emotion[9:]
@@ -103,8 +119,12 @@ def main():
         screen.fill((0,0,0))
 
         
+        
         #draw pale pink rectangle in the middle of the screen
         pg.draw.rect(screen, screen_bg_color, screen_rect, 0)
+        
+        # input_box background
+        pg.draw.rect(screen, (255,255,255), input_box, 0)
         
         #draw crapo bubble as a rounded rectangle
         pg.draw.rect(screen, (255,255,255), crapo_bubble, 0, 25)
@@ -119,12 +139,11 @@ def main():
         
         txt_surface = font.render(displayedText, True, color)
 
-        # Resize the box if the text is too long.
-        width = 300
-        input_box.w = width
+
 
         # Blit the text.
         screen.blit(txt_surface, (500, 600))
+
         # Blit the input_box rect.
         pg.draw.rect(screen, color, input_box, 2)
 
@@ -149,7 +168,7 @@ def main():
             pg.display.flip()
             
             # Blit the response.
-            screen.blit(resp_surface, (500, 150+j*50))
+            screen.blit(resp_surface, (500, 110+j*35))
             j+=1
             
         pg.display.flip()
